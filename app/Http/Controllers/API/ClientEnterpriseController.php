e1200c2e38f78dba91a17f6a9a3d4432351c5374
Raul <raul.marx.rm@gmail.com>
2024-06-13 10:45:22 -0300

    removendo job

--- app/Http/Controllers/API/ClientEnterpriseController.php
+++ app/Http/Controllers/API/ClientEnterpriseController.php
@@ -69,30 +69,28 @@ class ClientEnterpriseController extends Controller
         };
         $cnpj = $request->input('cnpj');
 
-        $result = $this->cnpjService->checkCNPJ($cnpj);
-
         $clientEnterprise = $this->ClientEnterpriseRepository->create([
             'cnpj' => $cnpj,
-            'name' => $request->input('name') ?? $result['data']['nome'],
-            'fantasy_name' => $request->input('fantasy_name') ?? $result['data']['fantasia'],
-            'address' => $request->input('address') ?? $result['data']['logradouro'],
-            'headquarters_unit' => $request->input('headquarters_unit') ?? $result['data']['descricao_matriz_filial'],
-            'commercial_phone' => $request->input('commercial_phone') ?? $result['data']['telefone'],
-            'commercial_email' => $request->input('commercial_email') ?? $result['data']['email'],
-            'employee_count' => $request->input('employee_count') ?? $result['data']['qtd_funcionarios'],
-            'company_size' => $request->input('company_size') ?? $result['data']['situacao'],
-            'business_segment' => $request->input('business_segment') ?? $result['data']['descricao_tipo_empresa'],
-            'company_profile' => $request->input('company_profile') ?? $result['data']['descricao_atividade_principal'],
-            'structured_hr_department' => $request->input('structured_hr_department') ?? $result['data']['setor_atividade'],
-            'responsible_name' => $request->input('responsible_name') ?? $result['data']['nome_responsavel'],
-            'responsible_email' => $request->input('responsible_email') ?? $result['data']['email_responsavel'],
-            'responsible_whatsapp' => $request->input('responsible_whatsapp') ?? $result['data']['telefone_responsavel'],
-            'responsible_phone' => $request->input('responsible_phone') ?? $result['data']['celular_responsavel'],
-            'mission' => $request->input('mission') ?? $result['data']['missao'],
-            'values' => $request->input('values') ?? $result['data']['visao'],
-            'pdi_program' => $request->input('pdi_program') ?? $result['data']['programa_pdhi'],
-            'work_regimes' => $request->input('work_regimes') ?? $result['data']['regime_trabalho'],
-            'profile_image_path' => $request->input('profile_image_path') ?? $result['data']['logo']
+            'name' => $request->input('name') ,
+            'fantasy_name' => $request->input('fantasy_name') ,
+            'address' => $request->input('address') ,
+            'headquarters_unit' => $request->input('headquarters_unit') ,
+            'commercial_phone' => $request->input('commercial_phone') ,
+            'commercial_email' => $request->input('commercial_email') ,
+            'employee_count' => $request->input('employee_count') ,
+            'company_size' => $request->input('company_size') ,
+            'business_segment' => $request->input('business_segment') ,
+            'company_profile' => $request->input('company_profile') ,
+            'structured_hr_department' => $request->input('structured_hr_department') ,
+            'responsible_name' => $request->input('responsible_name') ,
+            'responsible_email' => $request->input('responsible_email') ,
+            'responsible_whatsapp' => $request->input('responsible_whatsapp') ,
+            'responsible_phone' => $request->input('responsible_phone') ,
+            'mission' => $request->input('mission') ,
+            'values' => $request->input('values') ,
+            'pdi_program' => $request->input('pdi_program') ,
+            'work_regimes' => $request->input('work_regimes') ,
+            'profile_image_path' => $request->input('profile_image_path') ,
 
         ]);
 
